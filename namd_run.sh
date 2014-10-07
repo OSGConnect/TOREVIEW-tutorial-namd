f34a8cf2fd63c75503c97f816c2aac7c27624480
Balamurugan Desinghu <bala@nsit-dhcp-250-206.uchicago.edu>
2014-10-07 15:18:38 -0500

    namd tutorial for rcc connect

--- namd_run.sh
+++ namd_run.sh
@@ -1,4 +1,9 @@
+#!/bash/bin
 source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
 module load namd/2.9
 namd2 ubq_gbis_eq.conf > ubq_gbis_eq.log 
-
+mkdir OutFilesFromNAMD
+cp *.log OutFilesFromNAMD/.
+cp *restart* OutFilesFromNAMD/.
+cp *.out OutFilesFromNAMD/.
+tar cvzf OutFilesFromNAMD.tar.gz OutFilesFromNAMD
