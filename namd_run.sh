36ca7696e240642f76be6635365238d895103b4f
bala <dmbala@gmail.com>
2014-11-12 09:18:00 -0600

    simplified scripts

--- namd_run.sh
+++ namd_run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
 module load namd/2.9
-namd2 ubq_gbis_eq.conf > ubq_gbis_eq.log 
-mkdir OutFilesFromNAMD
-cp *.log OutFilesFromNAMD/.
-cp *restart* OutFilesFromNAMD/.
-cp *.out OutFilesFromNAMD/.
-tar cvzf OutFilesFromNAMD.tar.gz OutFilesFromNAMD
+namd2 ubq_gbis_eq.conf > output.$1.dat
+
+
+
