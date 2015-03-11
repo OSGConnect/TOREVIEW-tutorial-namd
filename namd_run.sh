8e45ed6ec9f15a5e2d610957c0930b276faf74be
bala <dmbala@gmail.com>
2015-03-11 14:25:52 -0500

    remove the module source line

--- namd_run.sh
+++ namd_run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
 module load namd/2.9
-namd2 ubq_gbis_eq.conf > output.$1.dat
+namd2 ubq_gbis_eq.conf > ubq_gbis_eq.$1.log
 
 
 
